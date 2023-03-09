059032a0ce19037ea82f49611401513ff519628b
mulchus <mulch@list.ru>
2023-03-10 00:17:57 +0300

    get title, img, description (dont work) of book

--- main.py
+++ main.py
@@ -45,17 +45,28 @@ def save_book(book_id, file_dir):
     if check_for_redirect(book_page):
         print('Книга не найдена')
 
-    book_id = Path(book_page_url).name[1:]
+    # book_id = Path(book_page_url).name[1:]
     txt_book_url = f'{book_file_basis_url}{book_id}'
 
     txt_book = requests.get(txt_book_url)
     txt_book.raise_for_status()
     if 'Content-Disposition' in txt_book.headers:
         # get page title
-        page_content = BeautifulSoup(book_page.text, 'html.parser')
-        for title in page_content.find_all('title'):
-            page_title = title.get_text()
-            print(page_title)
+        page_content = BeautifulSoup(book_page.text, 'lxml')
+
+        page_title = page_content.find('head').find('title')
+        print(page_title.text)
+
+        img_url = f"https://tululu.org{page_content.find('div', class_='bookimage').find('img')['src']}"
+        print(img_url)
+
+        book_description = page_content.find('table', class_='d_book').find('table', class_='d_book').find('td')
+        print(book_description)
+
+        # for title in page_content.find_all('title'):
+        #     page_title = title.get_text()
+        #     print(page_title)
+
         if 'filename' in txt_book.headers['Content-Disposition']:
             with open(file_dir.joinpath(f'{book_id}.txt'), 'wb') as file:
                 file.write(txt_book.content)
