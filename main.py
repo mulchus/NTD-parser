2a7deab00bf8bf6a8de4d59df6c701f0104f532c
mulchus <mulch@list.ru>
2023-03-13 21:18:46 +0300

    Dried some code

--- main.py
+++ main.py
@@ -15,35 +15,36 @@ IMAGE_DIR = 'Images'
 
 
 def main():
-    award_parser = argparse.ArgumentParser(description='Скрипт скачивания книг с сайта https://tululu.org/')
-    award_parser.add_argument(
-        'start_id',
+    book_parser = argparse.ArgumentParser(description='Скрипт скачивания книг с сайта https://tululu.org/')
+    book_parser.add_argument(
+        'start_book_id',
         nargs='?',
         default=1,
         help='начальный номер книги'
     )
-    award_parser.add_argument(
-        'end_id',
+    book_parser.add_argument(
+        'end_book_id',
         nargs='?',
         default=2,
         help='конечный номер книги'
     )
 
-    start_id = int(award_parser.parse_args().start_id)
-    end_id = int(award_parser.parse_args().end_id)
-    if start_id < 0 or end_id < 0:
+    book_parser_args = book_parser.parse_args()
+    start_book_id = int(book_parser_args.start_book_id)
+    end_book_id = int(book_parser_args.end_book_id)
+    if start_book_id < 0 or end_book_id < 0:
         sys.exit('Неверно введены ID книг')
-    if start_id > end_id:
-        start_id, end_id = end_id, start_id
+    if start_book_id > end_book_id:
+        start_book_id, end_book_id = end_book_id, start_book_id
 
-    print(f'Ищем книги с ID от {start_id} по {end_id}')
+    print(f'Ищем книги с ID от {start_book_id} по {end_book_id}')
 
     Path.cwd().joinpath(FILE_DIR).mkdir(parents=True, exist_ok=True)
     Path.cwd().joinpath(IMAGE_DIR).mkdir(parents=True, exist_ok=True)
 
     book_file_basis_url = 'https://tululu.org/txt.php?id='
 
-    for book_id in range(start_id, end_id+1):
+    for book_id in range(start_book_id, end_book_id+1):
         print('\n', f'book_id = {book_id}')
         txt_book_url = f'{book_file_basis_url}{book_id}'
         txt_book = requests.get(txt_book_url)
@@ -54,7 +55,6 @@ def main():
 
         if check_for_redirect(book_page):
             print('Книга не найдена')
-            # print()
             continue
 
         if 'Content-Disposition' in txt_book.headers:
@@ -116,7 +116,6 @@ def download_txt(txt_book, filename, folder='Books'):
 
 def download_image(img_url, filename, folder='Images'):
     folder, filename = folder_filename_validate(folder, filename)
-    # _, file_ext = os.path.splitext(img_url)
     _, filename = os.path.split(parse.urlsplit(parse.unquote(img_url)).path)
     filepath = os.path.join(folder, filename)
     image = requests.get(img_url)
@@ -128,10 +127,7 @@ def download_image(img_url, filename, folder='Images'):
 def check_for_redirect(book_page):
     book_page.raise_for_status()
     if book_page.url == 'https://tululu.org/':
-        try:
-            raise requests.HTTPError('Error page', book_page.request)
-        except requests.HTTPError as error:
-            return error
+        return requests.HTTPError('Error page', book_page.request)
 
 
 if __name__ == '__main__':
