b24d227e0dc5fa6f447a1f6788174f282cc6bf64
mulchus <mulch@list.ru>
2023-03-13 23:43:34 +0300

    Dried some code

--- main.py
+++ main.py
@@ -88,15 +88,15 @@ def parse_book_page(page_content):
 
     book_genres = []
     if page_content.find('span', class_='d_book'):
-        book_genres = page_content.find('span', class_='d_book').find('b').find_next_siblings('a')
-        book_genres = [genre.text for genre in book_genres]
+        book_genres = [genre.text for genre in page_content.find('span', class_='d_book').find('b')
+        .find_next_siblings('a')]
 
     book_img_url = f"https://tululu.org{page_content.find('div', class_='bookimage').find('img')['src']}"
 
     comments = []
     if page_content.find('div', class_='texts'):
-        comments = page_content.find('div', class_='texts').find_all_next('span', class_='black')
-        comments = [comment.text for comment in comments]
+        comments = [comment.text for comment in page_content.find('div', class_='texts')
+        .find_all_next('span', class_='black')]
 
     book_description = page_content.find_all('table', class_='d_book')[1].find('td').text
 
