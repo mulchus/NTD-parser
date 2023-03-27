270dbf98168e5525d685d00b627a7fede2a21e4f
Юрий <mulch@list.ru>
2023-03-27 12:34:10 +0300

    Made parsing all new NTD

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import argparse
-import book
-import parse_tululu_category
+import parse_all_ntd
 import json
 import time
 import requests
@@ -8,106 +6,37 @@ import requests
 from pathlib import Path
 
 
-FILE_DIR = 'Books'
-IMAGE_DIR = 'Images'
-PAGE_OF_CATEGORY_URL = 'https://tululu.org/l55/'
+URL_START_PAGE_OF_NTD = 'https://protect.gost.ru/default.aspx?control=6&month=3&year=2023'
+URL_START_PAGE_OF_NTD_PROJECT = ''
+WORD_ROOT_FILTER = ('геодез', 'геолог', 'эколог', 'гидромет', 'почв', 'проект', 'изыск', 'GPS', )
+
 
 
 def main():
-    parser = argparse.ArgumentParser(description='Скрипт скачивания книг с сайта https://tululu.org/')
-    parser.add_argument(
-        '--start_page',
-        nargs='?',
-        type=int,
-        default=1,
-        help='начальный номер книги'
-    )
-    parser.add_argument(
-        '--end_page',
-        nargs='?',
-        type=int,
-        default=0,
-        help='конечный номер книги'
-    )
-    parser.add_argument(
-        '--dest_folder',
-        nargs='?',
-        type=Path,
-        default=Path.cwd(),
-        help='путь к каталогу с результатами парсинга: картинкам, книгам, JSON'
-    )
-    parser.add_argument(
-        '--skip_imgs',
-        action="store_true",
-        help='не скачивать картинки'
-    )
-    parser.add_argument(
-        '--skip_txt',
-        action="store_true",
-        help='не скачивать книги'
-    )
-    parser.add_argument(
-        '--json_path',
-        nargs='?',
-        type=Path,
-        help='указать свой путь к *.json файлу с результатами'
-    )
 
-    parser_args = parser.parse_args()
-
-    if not parser_args.skip_txt:
-        Path.cwd().joinpath(parser_args.dest_folder, FILE_DIR).mkdir(parents=True, exist_ok=True)
-    if not parser_args.skip_imgs:
-        Path.cwd().joinpath(parser_args.dest_folder, IMAGE_DIR).mkdir(parents=True, exist_ok=True)
-    if parser_args.skip_txt and parser_args.skip_imgs:
-        parser_args.dest_folder.mkdir(parents=True, exist_ok=True)
-
-    page_of_category_url = PAGE_OF_CATEGORY_URL
+    # парсинг утвержденных НТД на Х месяц из URL_START_PAGE_OF_NTD
+    all_ntd = []
     while True:
         try:
-            books_urls = parse_tululu_category.get_books_urls(page_of_category_url,
-                                                              parser_args.start_page, parser_args.end_page)
+            all_ntd = parse_all_ntd.get_ntd(URL_START_PAGE_OF_NTD)
         except requests.exceptions.HTTPError as error:
-            print(f'Ошибка ссылки на категорию книг. Ошибка {error}')
+            print(f'Ошибка ссылки на страницу НТД. Ошибка {error}')
             break
         except requests.exceptions.ConnectionError as error:
             print(f'Ошибка сети. Ошибка {error}')
             time.sleep(1)
             continue
-
-        about_books = []
-        for book_page_url in books_urls:
-            while True:
-                try:
-                    filepath, about_book = book.get_book(book_page_url, parser_args)
-                    about_books.append(about_book)
-                    if filepath:
-                        print(f'Скачана книга: {filepath}')
-                    break
-                except requests.exceptions.ConnectionError as error:
-                    print(f'Ошибка сети. Ошибка {error}')
-                    time.sleep(1)
-                    continue
-                except requests.exceptions.HTTPError as error:
-                    if not error.errno:
-                        print(f'Неверная ссылка на страницу с книгой. Ошибка {error}')
-                    else:
-                        print(f'Нет файлов книги.')
-
-                    break
-
-        if not parser_args.skip_txt:
-            print(f'Скачано книг: ', {len(about_books)})
-
-        if not parser_args.json_path:
-            parser_args.json_path = parser_args.dest_folder
-        else:
-            Path.joinpath(parser_args.json_path).mkdir(parents=True, exist_ok=True)
-
-        with open(Path.joinpath(parser_args.json_path, 'about_books.json'), 'w', encoding='utf-8') as json_file:
-            json.dump(about_books, json_file, ensure_ascii=False, indent=4)
-
         break
+
+    print(f'Найдено стандартов (изменений): ', {len(all_ntd)})
+    with open(Path.joinpath(Path.cwd(), 'all_ntd.json'), 'w', encoding='utf-8') as json_file:
+        json.dump(all_ntd, json_file, ensure_ascii=False, indent=4)
+
+
+
+
+
+
 
 
 if __name__ == '__main__':
---
+++ parse_all_ntd.py
@@ -0,0 +1,54 @@
+import requests
+from main import WORD_ROOT_FILTER
+
+from bs4 import BeautifulSoup
+from urllib import parse
+
+
+def get_ntd(url_start_page_of_ntd):
+    all_ntd = []
+    splitresult = parse.urlsplit(url_start_page_of_ntd)
+    site_url = parse.urlunsplit([splitresult.scheme, splitresult.netloc, '', '', ''])
+    url_start_page_of_ntd_text = get_page(url_start_page_of_ntd, None).text
+    soup = BeautifulSoup(url_start_page_of_ntd_text, 'lxml')
+    last_page = int(soup.select('td.pages a')[-1].text)
+    for page in range(0, last_page):
+        current_page = url_start_page_of_ntd
+        payload = {
+            'page': page
+        }
+        page_of_ntds = get_page(current_page, payload)
+        page_of_ntds_content = BeautifulSoup(page_of_ntds.text, 'lxml')
+
+        for table in page_of_ntds_content.select('tr[bgcolor="#eeeeee"]'):
+
+            ntd_url = parse.urljoin('https://protect.gost.ru/', table.select_one('td div a')['href'])
+            ntd_number = table.select('td div[align="left"]')[0].text
+            ntd_name = table.select('td div[align="left"]')[1].text
+            if not ntd_name:
+                page_of_izm_ntd = get_page(ntd_url, '')
+                page_of_izm_ntd_content = BeautifulSoup(page_of_izm_ntd.text, 'lxml')
+                ntd_name = page_of_izm_ntd_content.select('td[colspan="3"]')[-2].select_one('tr.first').select('td')[-1].text
+
+            for root in WORD_ROOT_FILTER:
+                if root in ntd_name.lower():
+                    ntd = {
+                        'ntd_url': ntd_url,
+                        'ntd_number': ntd_number,
+                        'ntd_name': ntd_name,
+                    }
+                    all_ntd.append(ntd)
+
+    return all_ntd
+
+
+def get_page(page_url, payload):
+    page = requests.get(page_url, params=payload)
+    page.raise_for_status()
+    check_for_redirect(page)
+    return page
+
+
+def check_for_redirect(page):
+    if page.url == 'https://protect.gost.ru/':
+        raise requests.HTTPError('Err:01 - Нет информации на данной странице', page.request)
