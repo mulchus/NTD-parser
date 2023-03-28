f2fed597bafc44de2fea73a572456eb5a819131e
mulchus <mulch@list.ru>
2023-03-29 00:20:17 +0300

    Made parse_all_ntd_notifications.py to file (not to word) Update SEEKING_WORD_ROOTS

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import parse_all_ntd
+import parse_all_ntd_notifications
 import json
 import time
 import requests
@@ -6,14 +7,37 @@ from docxtpl import DocxTemplate, RichText
 from pathlib import Path
 
 
-URL_START_PAGE_OF_NTD = 'https://protect.gost.ru/default.aspx?control=6&month=2&year=2023'
-URL_START_PAGE_OF_NTD_PROJECTS = ''
-SEEKING_WORD_ROOTS = (' геодез', ' геолог', ' эколог', ' гидромет', ' почв', ' грунт', ' здани', ' сооружен', ' проект',
-                      ' изыск', ' репутац', ' градостр', ' город', 'gps', 'глонасс', 'cнип', ' сп ', 'сейсм',
+URL_START_PAGE_OF_NTD = '!!https://protect.gost.ru/default.aspx?control=6&month=2&year=2023'
+URL_START_PAGE_OF_NTD_NOTIFICATIONS = 'http://webportalsrv.gost.ru/portal/UVED_2007st.nsf/wMain?OpenView'
+SEEKING_WORD_ROOTS = ('геодез', 'геолог', 'эколог', 'гидромет', 'почв', 'грунт', 'здани', 'сооружен', 'проект',
+                      'изыск', 'репутац', 'градостр', 'город', 'gps', 'глонасс', 'cнип', 'сп ', 'сейсм',
                       'землетряс', 'земл', 'земн', 'обслед', )
 
 
 def main():
+    # парсинг проектов НТД на Х месяц из URL_START_PAGE_OF_NTD_PROJECTS
+    while True:
+        try:
+            all_notifications, all_notifications_for_table = \
+                parse_all_ntd_notifications.get_ntd_notifications(URL_START_PAGE_OF_NTD_NOTIFICATIONS)
+        except requests.exceptions.HTTPError as error:
+            print(f'Ошибка ссылки на страницу НТД. Ошибка {error}')
+            break
+        except requests.exceptions.ConnectionError as error:
+            print(f'Ошибка сети. Ошибка {error}')
+            time.sleep(1)
+            continue
+        break
+
+    print(f'Найдено уведомлений стандартов: {len(all_notifications)}')
+    with open(Path.joinpath(Path.cwd(), 'all_notifications_for_table.json'), 'w', encoding='utf-8') as json_file:
+        json.dump(all_notifications_for_table, json_file, ensure_ascii=False, indent=4)
+    with open(Path.joinpath(Path.cwd(), 'all_notifications.json'), 'w', encoding='utf-8') as json_file:
+        json.dump(all_notifications, json_file, ensure_ascii=False, indent=4)
+
+    exit()
+
+
     # парсинг утвержденных НТД на Х месяц из URL_START_PAGE_OF_NTD
     while True:
         try:
---
+++ parse_all_ntd_notifications.py
@@ -0,0 +1,72 @@
+import requests
+import main
+from datetime import datetime
+
+from bs4 import BeautifulSoup
+from urllib import parse
+
+
+def get_ntd_notifications(url_start_page_of_ntd_notifications):
+    all_notifications = []
+    all_notifications_for_table = {'tbl_contents':[]}
+    splitresult = parse.urlsplit(url_start_page_of_ntd_notifications)
+    site_url = parse.urlunsplit([splitresult.scheme, splitresult.netloc, '', '', ''])
+    start_page_of_ntd_notifications = get_page(url_start_page_of_ntd_notifications, None).text
+    start_page_content = BeautifulSoup(start_page_of_ntd_notifications, 'lxml')
+    current_month = int(start_page_content.select_one('tr[valign="top"] td a').text.split('/')[0])
+
+    ntd_notifications_first_in_page_number = 1
+    table_col_number = 1
+    while True:
+        payload = {
+            'Start': ntd_notifications_first_in_page_number
+        }
+        page_of_notifications = get_page(url_start_page_of_ntd_notifications, payload)
+        page_of_notifications_content = BeautifulSoup(page_of_notifications.text, 'lxml')
+
+        for table_row in page_of_notifications_content.select('tr[valign="top"]'):
+            notifications_public_date = table_row.select_one('td a').text
+
+            # проверка даты на предыдущий месяц и возврат при True
+            if int(notifications_public_date.split('/')[0]) < current_month:
+                return all_notifications, all_notifications_for_table
+
+            notifications_url = parse.urljoin('http://webportalsrv.gost.ru/', table_row.select_one('td a')['href'])
+            notifications_type = table_row.select('td')[1].text
+            ntd_name = table_row.select('td')[2].text
+
+            for root in main.SEEKING_WORD_ROOTS:
+                if root in ntd_name.lower():
+
+                    # результат в формате словаря
+                    ntd = {
+                        'notifications_public_date': notifications_public_date,
+                        'notifications_url': notifications_url,
+                        'notifications_type': notifications_type,
+                        'ntd_name': ntd_name,
+                    }
+
+                    # результат в формате словаря списков для таблицы
+                    notifications_for_table = {
+                        'cols': ['{{r ntd'+str(table_col_number)+'}}', notifications_type, ntd_name]
+                    }
+
+                    all_notifications.append(ntd)
+                    all_notifications_for_table['tbl_contents'].append(notifications_for_table)
+                    table_col_number += 1
+                    print(f'{notifications_public_date} - {ntd_name}')
+                    break
+
+        ntd_notifications_first_in_page_number += 29
+
+
+def get_page(page_url, payload):
+    page = requests.get(page_url, params=payload)
+    page.raise_for_status()
+    check_for_redirect(page)
+    return page
+
+
+def check_for_redirect(page):
+    if page.url == 'https://protect.gost.ru/':
+        raise requests.HTTPError('Err:01 - Нет информации на данной странице', page.request)
